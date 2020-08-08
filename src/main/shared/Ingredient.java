b2918b00440485ed6fa9bb94f239c0495ae824fc
Marwan Summakieh <42711091+MarwanSummakieh@users.noreply.github.com>
2020-08-08 10:17:15 +0200

    Update Ingredient.java

--- src/main/shared/Ingredient.java
+++ src/main/shared/Ingredient.java
@@ -5,19 +5,49 @@ import java.io.Serializable;
 public class Ingredient implements Serializable {
     private String name;
     private double price;
+    private int counter;
 
     public Ingredient(String name) {
+        counter = 1;
         this.name = name;
+        price = 0;
     }
 
-    public double getPrice(){return price; }
+    public Ingredient(String name, double price) {
+        counter = 1;
+        this.name = name;
+        this.price = price;
+    }
 
     public String getName() {
         return name;
     }
-    
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     @Override
     public String toString(){
-        return ", " + name;
+        if(counter == 0){
+            return "";
+        }else if(counter == 1){
+            return name + ", ";
+        }else {
+            return + counter + " " + name + ", ";
+        }
     }
+    
 }
